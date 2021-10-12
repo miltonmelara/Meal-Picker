6b7dd5a9f13a28c7120c6bb201ad429509325995
Milton Melara <milton.melara3b@gmail.com>
2021-10-12 17:58:36 -0600

    Added next part

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const meals = ['Mini Pizza','Dobladas', 'Bread with milk', 'French toasts', 'Sandwitch', 'Pasta', 'Mason jar with apple','Mason jar with strawberries', 'Banana shake', 'Red shake', ];
+//Dinner picker
+const dinners = ['Mini Pizza','Dobladas', 'French toasts', 'Sandwitch', 'Pasta', 'Mason jar with apple','Mason jar with strawberries', 'Banana shake', 'Purple shake', ];
+console.log(dinners.length)
 const button = document.getElementById('mealPicker');
-const meal = document.getElementById('meal');
+const dinner = document.getElementById('dinner');
+function pickDinner(){
+    const i = dinners[Math.floor(Math.random() * dinners.length)];
+    dinner.innerHTML = i;
+}
+button.addEventListener('click', pickDinner);
+
+//Mid day food picker
+const options = ['Mini Pizza','Dobladas', 'Sandwitch', 'Mason jar with apple','Mason jar with strawberries', 'Banana shake', 'Purple shake', 'bread with milk', 'cereal'];
+console.log(options.length)
+const button2 = document.getElementById('mealPicker2');
+const meal = document.getElementById('mid-day');
 function pickMeal(){
-    const i = meals[Math.floor(Math.random() * meals.length)];
+    const i = options[Math.floor(Math.random() * options.length)];
     meal.innerHTML = i;
 }
-button.addEventListener('click', pickMeal)
+button2.addEventListener('click', pickMeal);
